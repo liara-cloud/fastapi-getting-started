070be3d150799ddd43e23dfd651e4fce6cbb942c
Ali Najmabadi <alinajmabadizadeh2002@gmail.com>
2024-07-08 13:05:57 +0330

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+### How to run:
+
+To launch uvicorn:
+
+```
+uvicorn sql_app.main:app --reload
+```
---
+++ sql_app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DB_USER="sa"
+DB_NAME="dadaz"
+DB_HOST="bromo.liara.cloud"
+DB_PORT=31726
+DB_PASS="Lr0nm5zjaYwVTSgb6MKCJCWD"
+
+SQLALCHEMY_DATABASE_URL = (
+    f"mssql+pyodbc://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}?driver=ODBC+Driver+17+for+SQL+Server"
+)
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    # connect_args={"check_same_thread": False},  # only needed for SQLite
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
